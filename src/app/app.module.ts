c0f7bc0f0841100a495997a9977f211b1cd1c5fd
axkot <axkot@interia.pl>
2019-11-17 15:20:44 +0100

    Toolbar dodany

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -8,7 +9,7 @@ import { CarListComponent } from './car-list/car-list.component';
 import { CarDataService } from './car-data.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, MatToolbarModule ],
   declarations: [ AppComponent, HelloComponent, CarListComponent ],
   bootstrap:    [ AppComponent ],
   providers: [CarDataService]

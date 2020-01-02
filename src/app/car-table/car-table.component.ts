597e96a0f889bf32e804fb657790477409cfcc2e
axkot <axkot@interia.pl>
2020-01-02 21:28:10 +0100

    More summary columns

--- src/app/car-table/car-table.component.ts
+++ src/app/car-table/car-table.component.ts
@@ -3,11 +3,15 @@ import {Component} from '@angular/core';
 export interface PeriodicElement1 {
   model: string;
   brand: string;
+  average_month: number;
+  average_month_fuel: number;
+  average_month_service: number;
+  average_month_other: number;
 }
 
 const ELEMENT_DATA: PeriodicElement1[] = [
-  {brand: "Ford", model: 'Focus'},
-  {brand: "Renault", model: 'Scenic'}
+  {brand: "Ford", model: 'Focus', average_month: 100, average_month_fuel: 50, average_month_service: 40, average_month_other: 10},
+  {brand: "Renault", model: 'Scenic', average_month: 200, average_month_fuel: 50, average_month_service: 40, average_month_other: 110}
 ];
 
 
@@ -20,7 +24,7 @@ const ELEMENT_DATA: PeriodicElement1[] = [
   styleUrls: ['./car-table.component.css']
 })
 export class CarTableComponent {
-  displayedColumns: string[] = ['brand', 'model'];
+  displayedColumns: string[] = ['brand', 'model', 'average_month', 'average_month_fuel', 'average_month_service', 'average_month_other'];
   dataSource = ELEMENT_DATA;
 
   constructor() { }
